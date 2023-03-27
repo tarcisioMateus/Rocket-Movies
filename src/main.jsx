89603b25618f7aaa3a955336128580db63acfee2
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-03-27 20:42:58 -0300

    created styles/theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { ThemeProvider } from 'styled-components'
+import theme from '../src/styles/theme'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
 
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#0D0C0F",
+    BACKGROUND_800: "#1C1B1E",
+    BACKGROUND_700: "#262529",
+
+    WHITE: "#F4EDE8",
+    ORANGE: "#FF9000",
+
+    GRAY_100: "#CAC4CF",
+    GRAY_300: "#948F99",
+
+    PINK: "#FF859B",
+    PINKTRANSPARANT: "rgba(255, 133, 155, 0.05)",
+  },
+
+  FONTFAMILY: {
+    ROBOTO: "'Roboto', sans-serif",
+    SLAB: "'Roboto Slab', serif",
+
+  }
+}
