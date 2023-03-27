42cb8f239ea5256daf53df6fccafd30e70479a06
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-03-27 20:57:17 -0300

    created styles/global

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 
 import { ThemeProvider } from 'styled-components'
 import theme from '../src/styles/theme'
+import GlobalStyle from '../src/styles/global'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-
+      <GlobalStyle/>
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/styles/global.js
+++ src/styles/global.js
@@ -7,5 +7,42 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
-  
+  :root {
+    font-size: 62.5%;
+  }
+
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0,0,0,0);
+    white-space: nowrap;
+    border: none;
+  }
+
+  body {
+    background-color: ${ ({theme}) => theme.COLORS.BACKGROUND_800 };
+  }
+
+  input, body, button, a {
+    font-family: ${ ({theme}) => theme.FONTFAMILY.SLAB };
+    color: ${ ({theme}) => theme.COLORS.WHITE };
+    font-size: 1.6rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  a, button {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  a:hover, button:hover {
+    filter: brightness(0.9);
+  }
 `
