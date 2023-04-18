1b27a9be67dd264990ed5accc752ba19d73b8d46
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-04-17 21:45:55 -0300

    completed pages/Preview

--- src/pages/Preview/styles.js
+++ src/pages/Preview/styles.js
@@ -1,5 +1,72 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: grid;
+  grid-template-areas: "header" "content";
+  grid-template-rows: 11.6rem auto;
+`
 
+export const Content = styled.div`
+  grid-area: content ;
+  overflow-y: auto;
+  margin: 4rem 12.3rem 8rem;
+  padding-right: 1.6rem;
+
+  &::-webkit-scrollbar {
+    width: 0.8rem;
+  }
+ 
+  &::-webkit-scrollbar-track {
+    color: transparent;
+  }
+ 
+  &::-webkit-scrollbar-thumb {
+    background-color: ${ ({theme}) => theme.COLORS.PINK};
+    border-radius: 0.8rem;
+    outline: none;
+  }
+
+  .header {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    margin: 2.4rem 0;
+
+    >h2 {
+      font-weight: 500;
+      font-size: 3.6rem;
+      line-height: 4.7rem;
+    }
+  }
+
+  .tags {
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+    margin: 4rem 0;
+  }
+
+  >p {
+    font-size: 1.6rem;
+    line-height: 2.1rem;
+  }
+`
+
+export const TimeUser = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+  font-size: 1.6rem;
+
+  >img {
+    width: 1.6rem;
+    height: 1.6rem;
+    border-radius: 50%;
+  }
+
+  >svg {
+    color: ${ ({theme}) => theme.COLORS.PINK};
+  }
 `
