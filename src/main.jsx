cdb56e6c3d7f0936490d4864a599d38cc24cf7b3
Tarcisio Mateus <tarcisiomateus98@gmail.com>
2023-08-29 15:57:40 -0300

    routes added into AuthContext

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { ThemeProvider } from 'styled-components'
 import theme from '../src/styles/theme'
 import GlobalStyle from '../src/styles/global'
 
+import { AuthProvider } from './hooks/auth'
 import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Routes/>
+      <AuthProvider>
+        <Routes/>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
